1ef1c87603496ce07a68091f3c1e9c870ee11df7
Hxc <hxc19980624@163.com>
2018-11-23 20:20:13 +0800

    优化和扩展

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
-/*入口js*/
+/*
+入口JS
+ */
 import Vue from 'vue'
 import {Button} from 'mint-ui'
-import App from './App'
+import VueLazyload from 'vue-lazyload'
 
+import App from './App.vue'
 import store from './store'
 import router from './router'
-import TopHeader from './components/TopHeader/TopHeader'
-import Split from './components/Split/Split'
+import TopHeader from './components/TopHeader/TopHeader.vue'
+import Split from './components/Split/Split.vue'
+import loading from './common/imgs/loading.gif'
 
-//注册全局组件
+Vue.use(VueLazyload, { // 内部定义了一个全局指令lazy
+  loading,
+})
+
+// 注册全局组件
 Vue.component('TopHeader', TopHeader)
 Vue.component('Split', Split)
 Vue.component(Button.name, Button)
@@ -16,11 +24,13 @@ Vue.component(Button.name, Button)
 import './mock/mockServer'
 import './filters'
 
-/* eslint-disable no-new*/
-new Vue ({
+// 将store保存到Vue上
+Vue.store = store
+
+/* eslint-disable no-new */
+new Vue({
   el: '#app',
   render: h => h(App),
   router,
   store
 })
-
